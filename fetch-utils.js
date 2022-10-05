bf8c7dfb2512e795ea657ec1378f5116041c9fc5
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-10-04 20:26:05 -0500

    post function sending to supabase minus image

--- fetch-utils.js
+++ fetch-utils.js
@@ -28,3 +28,23 @@ export async function signOutUser() {
 }
 
 /* Data functions */
+
+export async function uploadImage(bucketName, imagePath, imageFile) {
+    const bucket = client.storage.from(bucketName);
+
+    const response = await bucket.upload(imagePath, imageFile, {
+        cacheControl: '3600',
+        upsert: true,
+    });
+
+    if (response.error) {
+        return null;
+    }
+
+    const url = `${SUPABASE_URL}/storage/v1/object/public/${response.data.Key}`;
+    return url;
+}
+
+export async function createRow(post) {
+    return await client.from('board').insert(post);
+}
--- newpost/post.js
+++ newpost/post.js
@@ -1,9 +1,18 @@
 import '../auth/user.js';
+import { uploadImage, createRow } from '../fetch-utils.js';
 
 /* Elements */
 
 const imgInput = document.getElementById('image-upload');
 const preview = document.getElementById('preview');
+const errorD = document.getElementById('error');
+const postForm = document.getElementById('post-form');
+
+/* state */
+
+let error = '';
+
+/* Event Listeners */
 
 imgInput.addEventListener('change', () => {
     const file = imgInput.files[0];
@@ -13,3 +22,43 @@ imgInput.addEventListener('change', () => {
         preview.src = '../assets/html.png';
     }
 });
+
+postForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(postForm);
+
+    const imageFile = formData.get('image');
+    const randomFolder = Math.floor(Date.now() * Math.random());
+    const imagePath = `bulletin/${randomFolder}/${imageFile.name}`;
+
+    const url = await uploadImage('bulletin', imagePath, imageFile);
+
+    const post = {
+        title: formData.get('title'),
+        descript: formData.get('post-text'),
+        category: formData.get('post-type'),
+        contact: formData.get('contact'),
+        image_url: url,
+    };
+
+    const response = await createRow(post);
+
+    error = response.error;
+
+    if (error) {
+        dispError();
+    } else {
+        location.assign('../');
+    }
+});
+
+/* Functions */
+
+function dispError() {
+    if (error) {
+        errorD.textContent = error.message;
+    } else {
+        errorD.textContent = '';
+    }
+}
